1ab9115f0427a9c1007ef0d18085836953c8fb11
arao <araobp@users.noreply.github.com>
2021-01-14 20:24:11 +0900

    update

---
+++ unity/BabyStroller/Assets/Door/SafetySensor.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SafetySensor : MonoBehaviour
+{
+    public bool enabled = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log(collision);
+        if (enabled)
+        {
+            gameObject.GetComponentInParent<Animator>().SetTrigger("Open");
+        }
+    }
+}
