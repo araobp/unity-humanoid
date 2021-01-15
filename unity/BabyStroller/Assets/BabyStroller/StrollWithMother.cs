003c7e000bfcb58ea5b0fd6049c71f8ea7c7ae6c
arao <araobp@users.noreply.github.com>
2021-01-15 10:39:21 +0900

    update

--- unity/BabyStroller/Assets/BabyStroller/StrollWithMother.cs
+++ unity/BabyStroller/Assets/BabyStroller/StrollWithMother.cs
@@ -8,18 +8,11 @@ public class StrollWithMother : MonoBehaviour
     public GameObject mother;
     public float distanceFromMother = 0.5F;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
     // Update is called once per frame
     void Update()
     {
         Vector3 pos = mother.transform.position + mother.transform.forward * distanceFromMother;
-
         transform.position = new Vector3(pos.x, transform.position.y, pos.z);
-        transform.Rotate(transform.up, mother.transform.rotation.y - transform.rotation.y);
+        transform.rotation = Quaternion.Euler(transform.rotation.eulerAngles.x, mother.transform.rotation.eulerAngles.y, transform.rotation.eulerAngles.z);
     }
 }
