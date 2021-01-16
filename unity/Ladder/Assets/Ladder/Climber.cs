3f785bf7d51dd02008502ba42d543872dbfbbf65
arao <araobp@users.noreply.github.com>
2021-01-16 23:42:11 +0900

    Ladder climbing

---
+++ unity/Ladder/Assets/Ladder/Climber.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Climber : MonoBehaviour
+{
+    Animator animator;
+    Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other.name);
+        if (animator != null && other.name == "ladder")
+        {
+            animator.SetTrigger("OnTop");
+        }
+    }
+}
