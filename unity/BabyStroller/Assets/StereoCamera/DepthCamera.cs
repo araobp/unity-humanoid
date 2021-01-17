7cecd6bb5c1c97c321a37d1f7ed43ac68910fcf7
arao <araobp@users.noreply.github.com>
2021-01-17 12:52:57 +0900

    update

---
+++ unity/BabyStroller/Assets/StereoCamera/DepthCamera.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Text;
+using System.Threading;
+
+public class DepthCamera : MonoBehaviour
+{
+
+    public Material material;
+    Camera depthCamera;
+
+    public void OnRenderImage(RenderTexture source, RenderTexture dest)
+    {
+        Graphics.Blit(source, dest, material);
+    }
+
+    void Start()
+    {
+        depthCamera = GetComponent<Camera>();
+        depthCamera.depthTextureMode |= DepthTextureMode.Depth;
+    }
+
+}
