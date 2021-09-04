633f0f648943b803883343a1ba4319379970df84
araobp <araobp@users.noreply.github.com>
2021-09-04 16:12:44 +0900

    update

---
+++ unity/Station/Assets/WalkWithIk.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class WalkWithIk : MonoBehaviour
+{
+    public GameObject leftHand;
+
+    private Animator animator;
+    float leftHandPositionWeight = 0.9F;
+    float leftHandRotationWeight = 0.5F;
+    Transform leftHandObj;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        leftHandObj = leftHand.transform;
+    }
+
+    void OnAnimatorIK()
+    {
+        animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, leftHandPositionWeight);
+        animator.SetIKRotationWeight(AvatarIKGoal.LeftHand, leftHandRotationWeight);
+        animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandObj.position);
+        animator.SetIKRotation(AvatarIKGoal.LeftFoot, leftHandObj.rotation);
+    }
+}
